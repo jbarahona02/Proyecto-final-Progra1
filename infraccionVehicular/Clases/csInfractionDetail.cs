edeb954666f9666550f6651e40cd6a73bdd6eded
Raul Galdamez <ralgaldamez@gmail.com>
2022-04-05 21:15:56 -0600

    Infraction Detail Web Service Added

---
+++ infraccionVehicular/Clases/csInfractionDetail.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace infraccionVehicular.Clases
+{
+    public class csInfractionDetail
+    {
+
+        public DataSet infractionDetailList() {
+
+            DataSet ds = new DataSet();
+
+            try {
+
+                MySqlConnection cn = new MySqlConnection();
+
+                cn.ConnectionString = ConfigurationManager.ConnectionStrings["cnConnection"].ConnectionString;
+                cn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter("select * from infractiondetailid", cn);
+
+                da.Fill(ds);
+
+                cn.Close();
+
+            } catch (Exception e) { }
+
+            return ds;
+        
+        }
+
+        public DataSet infractionDetail(string id) {
+
+            DataSet ds = new DataSet();
+
+            try {
+
+                MySqlConnection cn = new MySqlConnection();
+
+                cn.ConnectionString = ConfigurationManager.ConnectionStrings["cnConnection"].ConnectionString;
+                cn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter("select * from infractiondetailid where id = " + id, cn);
+
+                da.Fill(ds);
+
+                cn.Close();
+
+            } catch (Exception e) { }
+
+            return ds;
+
+        }
+
+        public Int32 insertInfractionDetail(int infractionId, int sanctionId) {
+
+            Int32 respuesta = 0;
+
+            try {
+
+                MySqlConnection cn = new MySqlConnection();
+                cn.ConnectionString = ConfigurationManager.ConnectionStrings["cnConnection"].ConnectionString;
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into infractiondetailid (infractionId, sanctionId)" +
+                    " values (" + infractionId + ", " + sanctionId + ")", cn);
+
+                respuesta = cmd.ExecuteNonQuery();
+
+                cn.Close();
+
+            } catch (Exception e) { }
+
+            return respuesta;
+        
+        }
+
+        public Int32 updateInfractionDetail(int id, int infractionId, int sanctionId) {
+
+            Int32 respuesta = 0;
+
+            try {
+
+                MySqlConnection cn = new MySqlConnection();
+                cn.ConnectionString = ConfigurationManager.ConnectionStrings["cnConnection"].ConnectionString;
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("update infractiondetailid set infractionId = " + infractionId + 
+                    ", sanctionId = " + sanctionId + " where id = " + id, cn);
+
+                respuesta = cmd.ExecuteNonQuery();
+
+                cn.Close();
+
+            } catch(Exception e) { }
+
+            return respuesta;
+        
+        }
+
+        public Int32 deleteInfractionDetail(int id) {
+
+            Int32 respuesta = 0;
+
+            try {
+
+                MySqlConnection cn = new MySqlConnection();
+                cn.ConnectionString = ConfigurationManager.ConnectionStrings["cnConnection"].ConnectionString;
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delete from infractiondetailid where id = " + id, cn);
+
+                respuesta = cmd.ExecuteNonQuery();
+
+                cn.Close();
+
+            } catch(Exception e) { }
+
+            return respuesta;
+        
+        }
+
+    }
+}
---
+++ infraccionVehicular/wsInfractionDetail.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+
+namespace infraccionVehicular
+{
+    /// <summary>
+    /// Summary description for wsInfractionDetail
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class wsInfractionDetail : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public DataSet infractionDetailList() {
+            return new Clases.csInfractionDetail().infractionDetailList();
+        }
+
+        [WebMethod]
+        public DataSet infractionDetail(string id) {
+            return new Clases.csInfractionDetail().infractionDetail(id);
+        }
+
+        [WebMethod]
+        public Int32 insertInfractionDetail(int infractionId, int sanctionId) {
+            return new Clases.csInfractionDetail().insertInfractionDetail(infractionId, sanctionId);
+        }
+
+        [WebMethod]
+        public Int32 updateInfractionDetail(int id, int infractionId, int sanctionId) {
+            return new Clases.csInfractionDetail().updateInfractionDetail(id, infractionId, sanctionId);
+        }
+
+        [WebMethod]
+        public Int32 deleteInfractionDetail(int id) {
+            return new Clases.csInfractionDetail().deleteInfractionDetail(id);
+        }
+
+    }
+}
