8304656c381476e5d50bf4955e62c183c24303ca
jbarahona02 <jbarahona-2018064@kinal.edu.gt>
2022-05-20 19:33:25 -0600

    Web service report

---
+++ infraccionVehicular/wsInfractionReport.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="wsInfractionReport.asmx.cs" Class="infraccionVehicular.wsInfractionReport" %>
---
+++ infraccionVehicular/wsInfractionReport.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace infraccionVehicular
+{
+    /// <summary>
+    /// Descripción breve de wsInfractionReport
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class wsInfractionReport : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public DataSet infractionReport(int idInfraction)
+        {
+            return new Clases.csInfractionReport().infractionReport(idInfraction);
+        }
+    }
+}
